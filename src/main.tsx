c62f987e2b21e4769fee3509dac516d3054a3bf9
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-03-17 10:49:39 +0200

    add

--- src/main.tsx
+++ src/main.tsx
@@ -12,25 +12,37 @@ telegramAnalytics.init({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TonConnectUIProvider 
-        manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
-        uiPreferences={{
-          colorsSet: {
-            [THEME.DARK]: {
-              connectButton: {
-                background: '#141414',
-              },
-              accent: '#4a4a4a',
-              telegramButton: '#1a1a1a',
-              background: {
-                primary: '#0d0d0d',
-                secondary: '#1a1a1a',
-                tint: '#2b2b2b'
-              }
+      manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
+      uiPreferences={{
+        colorsSet: {
+          [THEME.DARK]: {
+            connectButton: {
+              background: '#141414',
+            },
+            accent: '#4a4a4a',
+            telegramButton: '#1a1a1a',
+            background: {
+              primary: '#0d0d0d',
+              secondary: '#1a1a1a',
+              tint: '#2b2b2b'
             }
           },
-        }}
-      >
-        <AppRouter />
-      </TonConnectUIProvider>
+          [THEME.LIGHT]: { 
+            connectButton: {
+              background: '#141414',
+            },
+            accent: '#4a4a4a',
+            telegramButton: '#1a1a1a',
+            background: {
+              primary: '#0d0d0d',
+              secondary: '#1a1a1a',
+              tint: '#2b2b2b'
+            }
+          }
+        },
+      }}
+    >
+      <AppRouter />
+    </TonConnectUIProvider>
   </StrictMode>,
 )
